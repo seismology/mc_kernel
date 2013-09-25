10c1b5a04fdf25542ff28a0de63a9f0b4ad25a4a
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2013-09-25 14:05:30 +0000

    - some more tetrahedral routines

--- random_tetrahedron/random_tetrahedron.f90
+++ random_tetrahedron/random_tetrahedron.f90
@@ -1,5 +1,6 @@
-module random_points
+module tetrahedra
 
+  integer, parameter :: dp = 8, sp = 4
 contains
 !*****************************************************************************80
 !
@@ -39,23 +40,19 @@ function generate_random_point(v, n)
     implicit none
 
     integer, intent(in)        ::  n
-    real(kind=sp), intent(in)  ::  v(3,4)
-    real(kind=sp), intent(out) ::  generate_random_point(3,n)
+    real(kind=dp), intent(in)  ::  v(3,4)
+    real(kind=dp)              ::  generate_random_point(3,n)
 
-    real(kind=sp)              ::  c(4)
+    real(kind=dp)              ::  c(4)
     real(kind=sp)              ::  coordinates(3,n)
     integer                    ::  j
     integer                    ::  seed
-    real(kind=8 )          ::  t
+    real(kind=dp )             ::  t
 
     call random_number(coordinates)
     do j = 1, n
 
         c(1:3) = dble(coordinates(:, j))
-      !call random_number(c(1))
-      !call random_number(c(2))
-      !call random_number(c(3))
-       !call r8vec_uniform_01 ( 3, seed, c )
 
         if ( 1.0D+00 < c(1) + c(2) ) then
             c(1) = 1.0D+00 - c(1)
@@ -76,11 +73,76 @@ function generate_random_point(v, n)
 !  
 !    C(1:4) are the barycentric coordinates of the point.
 !  
-        x(1:3,j) = real(matmul ( dble(v(1:3,1:4)), c(1:4) ))
+        generate_random_point(1:3,j) = matmul( v(1:3,1:4), dble(c(1:4)) )
 
     end do
 
     return
-end subroutine
+end function
 
+!*******************************************************************************
+
+function rmat4_det ( a )
+!! RMAT4_DET computes the determinant of a 4 by 4 matrix.
+  implicit none
+  real(kind=dp), intent(in)  :: a(4,4)
+  real(kind=dp)              :: rmat4_det
+
+  rmat4_det = &
+      a(1,1) * ( &
+        a(2,2) * ( a(3,3) * a(4,4) - a(3,4) * a(4,3) ) &
+      - a(2,3) * ( a(3,2) * a(4,4) - a(3,4) * a(4,2) ) &
+      + a(2,4) * ( a(3,2) * a(4,3) - a(3,3) * a(4,2) ) ) &
+    - a(1,2) * ( &
+        a(2,1) * ( a(3,3) * a(4,4) - a(3,4) * a(4,3) ) &
+      - a(2,3) * ( a(3,1) * a(4,4) - a(3,4) * a(4,1) ) &
+      + a(2,4) * ( a(3,1) * a(4,3) - a(3,3) * a(4,1) ) ) &
+    + a(1,3) * ( &
+        a(2,1) * ( a(3,2) * a(4,4) - a(3,4) * a(4,2) ) &
+      - a(2,2) * ( a(3,1) * a(4,4) - a(3,4) * a(4,1) ) &
+      + a(2,4) * ( a(3,1) * a(4,2) - a(3,2) * a(4,1) ) ) &
+    - a(1,4) * ( &
+        a(2,1) * ( a(3,2) * a(4,3) - a(3,3) * a(4,2) ) &
+      - a(2,2) * ( a(3,1) * a(4,3) - a(3,3) * a(4,1) ) &
+      + a(2,3) * ( a(3,1) * a(4,2) - a(3,2) * a(4,1) ) )
+
+end function rmat4_det
+
+!*******************************************************************************
+
+
+function tetra_volume_3d ( v )
+!
+!*******************************************************************************
+!
+!! TETRA_VOLUME_3D computes the volume of a tetrahedron in 3D.
+!
+!
+!  Modified:
+!
+!    06 February 1999
+!
+!  Author:
+!
+!    John Burkardt
+!
+  implicit none
+!
+  real(kind=dp), intent(in)  ::  v(3,4)
+  real(kind=dp)              ::  a(4,4)
+  real(kind=dp)              ::  tetra_volume_3d
+!
+  a(1:4,1:3) = transpose(v)
+!
+!  a(1:4,1) = (/ x1, x2, x3, x4 /)
+!
+!  a(1:4,2) = (/ y1, y2, y3, y4 /)
+!
+!  a(1:4,3) = (/ z1, z2, z3, z4 /)
+
+  a(1:4,4) = [1, 1, 1, 1]
+
+  tetra_volume_3d = abs ( rmat4_det ( a ) ) / 6.0d+00
+
+end function tetra_volume_3d
 end module
