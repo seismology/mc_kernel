6d93abefd918a5153f6e2c117eca278ff4848714
Martin van Driel <vandriel@erdw.ethz.ch>
2013-09-26 14:22:58 +0000

    - adding xdmf dump routine + test

--- inv_mesh/inv_mesh.f90
+++ inv_mesh/inv_mesh.f90
@@ -16,7 +16,7 @@ module inversion_mesh
      procedure, pass :: get_nelements
      procedure, pass :: get_nvertices
      procedure, pass :: read_tet_mesh
-     !procedure, pass :: dump_mesh_xdmf
+     procedure, pass :: dump_tet_mesh_xdmf
      procedure, pass :: freeme
   end type
 
@@ -43,8 +43,8 @@ end function
 !-----------------------------------------------------------------------------------------
 subroutine read_tet_mesh(this, filename_vertices, filename_connectivity)
   class(inversion_mesh_type)        :: this
-  character(len=*)                  :: filename_vertices
-  character(len=*)                  :: filename_connectivity
+  character(len=*), intent(in)      :: filename_vertices
+  character(len=*), intent(in)      :: filename_connectivity
   integer                           :: iinput_vertices, iinput_connectivity
   integer                           :: i
 
@@ -78,6 +78,58 @@ end subroutine
 !-----------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------
+subroutine dump_tet_mesh_xdmf(this, filename)
+  class(inversion_mesh_type)        :: this
+  character(len=*), intent(in)      :: filename
+  integer                           :: iinput_xdmf, iinput_heavy_data
+  integer                           :: i
+
+  if (.not. this%initialized) &
+     stop 'ERROR: trying to dump a non initialized mesh'
+
+  open(newunit=iinput_xdmf, file=trim(filename)//'.xdmf')
+  write(iinput_xdmf, 732) this%nelements, this%nelements, 'binary', &
+                      trim(filename)//'_grid.dat', this%nvertices, 'binary', &
+                      trim(filename)//'_points.dat'
+  close(iinput_xdmf)
+
+732 format(&    
+    '<?xml version="1.0" ?>',/&
+    '<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>',/&
+    '<Xdmf xmlns:xi="http://www.w3.org/2003/XInclude" Version="2.2">',/&
+    '<Domain>',/&
+    '<Grid Name="CellsTime" GridType="Collection" CollectionType="Temporal">',/&
+    '  <Grid GridType="Uniform">',/&
+    '    <Time Value="0.000" />',/&
+    '    <Topology TopologyType="Tetrahedron" NumberOfElements="',i10,'">',/&
+    '      <DataItem Dimensions="',i10,' 4" NumberType="Int" Format="',A,'">',/&
+    '        ',A,/&
+    '      </DataItem>',/&
+    '    </Topology>',/&
+    '    <Geometry GeometryType="XYZ">',/&
+    '      <DataItem Dimensions="',i10,' 3" NumberType="Float" Format="',A,'">',/&
+    '        ',A/&
+    '      </DataItem>',/&
+    '    </Geometry>',/&
+    '  </Grid>',/&
+    '</Grid>',/&
+    '</Domain>',/&
+    '</Xdmf>')
+
+  open(newunit=iinput_heavy_data, file=trim(filename)//'_points.dat', access='stream', &
+      status='replace', form='unformatted', convert='little_endian')
+  write(iinput_heavy_data) this%vertices
+  close(iinput_heavy_data)
+
+  open(newunit=iinput_heavy_data, file=trim(filename)//'_grid.dat', access='stream', &
+      status='replace', form='unformatted', convert='little_endian')
+  write(iinput_heavy_data) this%connectivity
+  close(iinput_heavy_data)
+  
+end subroutine
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
 subroutine freeme(this)
   class(inversion_mesh_type)        :: this
   deallocate(this%vertices)
@@ -93,7 +145,11 @@ program test_inversion_mesh
   use inversion_mesh
   type(inversion_mesh_type) :: inv_mesh
 
-  call inv_mesh%read_tet_mesh('vertices.TEST', 'facets.TEST')
+  !call inv_mesh%read_tet_mesh('vertices.TEST', 'facets.TEST')
+  call inv_mesh%read_tet_mesh('vertices.USA10', 'facets.USA10')
+
+  call inv_mesh%dump_tet_mesh_xdmf('testmesh')
+  
   call inv_mesh%freeme()
 
 end program
