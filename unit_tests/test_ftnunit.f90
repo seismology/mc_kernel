9d5c5e2f3b6222d4bfe425a2b4aa91f998b5fc4a
Martin van Driel <vandriel@erdw.ethz.ch>
2013-10-04 16:39:35 +0000

    - too long lines

--- unit_tests/test_ftnunit.f90
+++ unit_tests/test_ftnunit.f90
@@ -37,9 +37,12 @@ subroutine test_all
   call test(test_mesh_read, 'reading tetrahedral mesh')
   call test(test_mesh_dump, 'reading/dumping tetrahedral mesh')
   call test(test_mesh_data_dump, 'reading/dumping tetrahedral mesh with data')
-  call test(test_mesh_data_dump2, 'reading/dumping triangular mesh from abaqus file with data')
-  call test(test_mesh_data_dump3, 'reading/dumping quadrilateral mesh from abaqus file with data')
-  call test(test_mesh_data_dump4, 'reading/dumping hexahedral mesh from abaqus file with data')
+  call test(test_mesh_data_dump2, &
+            'reading/dumping triangular mesh from abaqus file with data')
+  call test(test_mesh_data_dump3, &
+            'reading/dumping quadrilateral mesh from abaqus file with data')
+  call test(test_mesh_data_dump4, &
+            'reading/dumping hexahedral mesh from abaqus file with data')
   call test(test_valence, 'computation of valence')
   call test(test_get_connected_elements, 'get connected elements')
 end subroutine
