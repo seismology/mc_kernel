486d4f055823e63b3b545856cfdec2f30a08cb10
sstaehler <staehler@geophysik.uni-muenchen.de>
2014-04-04 13:30:57 +0200

    Write out STF for debugging purposes

--- filtering.f90
+++ filtering.f90
@@ -141,12 +141,21 @@ subroutine add_stfs(this, stf_fwd, stf_bwd)
     close(10)
     
 21  format('stf_spectrum_', A, 2('_', F0.3))
-22  format(5(E15.8))
+22  format(5(E16.8))
     write(fnam,21) trim(this%filterclass), this%frequencies(1:2)
     open(10, file=trim(fnam), action='write')
     do ifreq = 1, this%nfreq
         write(10,22), this%f(ifreq), real(stfs_fd(ifreq,1)), imag(stfs_fd(ifreq,1)), &
                                      real(stfs_fd(ifreq,2)), imag(stfs_fd(ifreq,2))
+    end do
+    close(10)
+    
+23  format('stf_', A, 2('_', F0.3))
+24  format(3(E16.8))
+    write(fnam,23) trim(this%filterclass), this%frequencies(1:2)
+    open(10, file=trim(fnam), action='write')
+    do ifreq = 1, size(stf_fwd)
+       write(10,24), real(ifreq), stfs(ifreq,1), stfs(ifreq,1)
     end do
     close(10)
     
