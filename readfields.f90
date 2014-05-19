6d9d17782bd0d0cf1ca443e2a794e7565d5f43cb
Martin van Driel <vandriel@erdw.ethz.ch>
2014-05-19 14:46:32 +0200

    finally getting azimuthal prefactors correct, so N and E
    component should be fine in rdbm now

--- readfields.f90
+++ readfields.f90
@@ -1055,7 +1055,6 @@ function load_fw_points_rdbm(this, source_params, reci_source_params, component)
 
                ! rotate source mt to receiver s,phi,z system
                mij_buff = rotate_symm_tensor_voigt_xyz_to_src_1d(mij_buff, rotmesh_phi(ipoint))
-               !mij_buff = rotate_symm_tensor_voigt_src_to_xyz_1d(mij_buff, rotmesh_phi(ipoint))
 
                write(6,'(A,6E12.2)') 'xyz_to_src           ', mij_buff
 
@@ -1069,41 +1068,185 @@ function load_fw_points_rdbm(this, source_params, reci_source_params, component)
                   load_fw_points_rdbm(:, 1, ipoint) = &
                         load_fw_points_rdbm(:, 1, ipoint) &
                             + mij_buff(i) * utemp(:,i)
-                  !@TODO I have the impression I am missing a factor of two for the
-                  !      components 4-6 here
                enddo 
 
+               ! components 4-6 need a factor of two because of voigt mapping
+               ! without factor of two in the strain
                do i = 4, 6
                   load_fw_points_rdbm(:, 1, ipoint) = &
                         load_fw_points_rdbm(:, 1, ipoint) &
                             + 2 * mij_buff(i) * utemp(:,i)
                enddo 
 
-               write(6,*) '--------------------------------------------------'
-               ! for testing rotate back:
-               !mij_buff = mij_buff * this%fwd(isim)%amplitude
-               !mij_buff = (/1, 0, 0, 0, 0, 0 /) * this%fwd(isim)%amplitude
-               !write(6,*) mij_buff
+               write(6,*) '=================================================='
 
-               !mij_buff = rotate_symm_tensor_voigt_src_to_xyz_1d(mij_buff, rotmesh_phi(ipoint))
-               !write(6,*) mij_buff
+          case('N')
+               isim = 2
+               utemp = load_strain_point(this%fwd(isim),      &
+                                         pointid(ipoint),     &
+                                         this%model_param)
 
-               !mij_buff = rotate_symm_tensor_voigt_xyz_src_to_xyz_earth_1d( &
-               !                 mij_buff, &
-               !                 reci_source_params%lon, &
-               !                 reci_source_params%colat)
-               !write(6,*) mij_buff
+               write(6,*) '=================================================='
+               write(6,*) reci_source_params%colat * rad2deg
+               write(6,*) rotmesh_phi(ipoint) * rad2deg
 
-               !mij_buff = rotate_symm_tensor_voigt_xyz_src_to_xyz_earth_1d( &
-               !                 mij_buff, &
-               !                 source_params(ipoint)%lon, &
-               !                 source_params(ipoint)%colat)
-               !write(6,*) mij_buff
+               write(6,'(A,6E12.2)') 'input                ', source_params(ipoint)%mij_voigt
+               ! rotate source mt to global cartesian system
+               mij_buff = rotate_symm_tensor_voigt_xyz_src_to_xyz_earth_1d( &
+                                source_params(ipoint)%mij_voigt, &
+                                source_params(ipoint)%lon, &
+                                source_params(ipoint)%colat)
+               write(6,'(A,6E12.2)') 'xyz_src_to_xyz_earth ', mij_buff
+
+               ! rotate source mt to receiver cartesian system
+               mij_buff = rotate_symm_tensor_voigt_xyz_earth_to_xyz_src_1d( &
+                                mij_buff, &
+                                reci_source_params%lon, &
+                                reci_source_params%colat)
+               write(6,'(A,6E12.2)') 'xyz_earth_to_xyz_src ', mij_buff
+
+               ! rotate source mt to receiver s,phi,z system
+               mij_buff = rotate_symm_tensor_voigt_xyz_to_src_1d(mij_buff, rotmesh_phi(ipoint))
+
+               write(6,'(A,6E12.2)') 'xyz_to_src           ', mij_buff
+
+               mij_buff = mij_buff / this%fwd(isim)%amplitude
+               write(6,*)
+               write(6,'(A,6E12.2)') 'scaled               ', mij_buff
+
+               load_fw_points_rdbm(:, :, ipoint) = 0
+               
+               !mij_buff = [1, 0, 0, 0, 0, 0]
+               !mij_buff = [0, 1, 0, 0, 0, 0]
+               !mij_buff = [0, 0, 1, 0, 0, 0]
+               !mij_buff = [0, 0, 0, 1, 0, 0]
+               !mij_buff = [0, 0, 0, 0, 1, 0]
+               !mij_buff = [0, 0, 0, 0, 0, 1]
+               !do i = 1, 3
+               !   load_fw_points_rdbm(:, 1, ipoint) = &
+               !         load_fw_points_rdbm(:, 1, ipoint) &
+               !             + mij_buff(i) * utemp(:,i)
+               !enddo 
+
+               !! components 4-6 need a factor of two because of voigt mapping
+               !! without factor of two in the strain
+               !do i = 4, 6
+               !   load_fw_points_rdbm(:, 1, ipoint) = &
+               !         load_fw_points_rdbm(:, 1, ipoint) &
+               !             + 2 * mij_buff(i) * utemp(:,i)
+               !enddo 
+               !
+               !load_fw_points_rdbm(:, 1, ipoint) = &
+               !        - load_fw_points_rdbm(:, 1, ipoint)  &
+               !             * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 1) 
+
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(1) * utemp(:,1) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(2) * utemp(:,2) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(3) * utemp(:,3) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(4) * utemp(:,4) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 2) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(5) * utemp(:,5) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(6) * utemp(:,6) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 1d0, 0d0/), isim, 2) 
+               
+               !@TODO not sure why we need the - sign here
+               load_fw_points_rdbm(:, 1, ipoint) = - load_fw_points_rdbm(:, 1, ipoint)
+
+               write(6,*) '=================================================='
+
+          case('E')
+               isim = 2
+               utemp = load_strain_point(this%fwd(isim),      &
+                                         pointid(ipoint),     &
+                                         this%model_param)
+
+               write(6,*) '=================================================='
+               write(6,*) reci_source_params%colat * rad2deg
+               write(6,*) rotmesh_phi(ipoint) * rad2deg
+
+               write(6,'(A,6E12.2)') 'input                ', source_params(ipoint)%mij_voigt
+               ! rotate source mt to global cartesian system
+               mij_buff = rotate_symm_tensor_voigt_xyz_src_to_xyz_earth_1d( &
+                                source_params(ipoint)%mij_voigt, &
+                                source_params(ipoint)%lon, &
+                                source_params(ipoint)%colat)
+               write(6,'(A,6E12.2)') 'xyz_src_to_xyz_earth ', mij_buff
+
+               ! rotate source mt to receiver cartesian system
+               mij_buff = rotate_symm_tensor_voigt_xyz_earth_to_xyz_src_1d( &
+                                mij_buff, &
+                                reci_source_params%lon, &
+                                reci_source_params%colat)
+               write(6,'(A,6E12.2)') 'xyz_earth_to_xyz_src ', mij_buff
+
+               ! rotate source mt to receiver s,phi,z system
+               mij_buff = rotate_symm_tensor_voigt_xyz_to_src_1d(mij_buff, rotmesh_phi(ipoint))
+
+               write(6,'(A,6E12.2)') 'xyz_to_src           ', mij_buff
+
+               mij_buff = mij_buff / this%fwd(isim)%amplitude
+               write(6,*)
+               write(6,'(A,6E12.2)') 'scaled               ', mij_buff
+
+               load_fw_points_rdbm(:, :, ipoint) = 0
+               
+               !do i = 1, 3
+               !   load_fw_points_rdbm(:, 1, ipoint) = &
+               !         load_fw_points_rdbm(:, 1, ipoint) &
+               !             + mij_buff(i) * utemp(:,i)
+               !enddo 
+
+               !! components 4-6 need a factor of two because of voigt mapping
+               !! without factor of two in the strain
+               !do i = 4, 6
+               !   load_fw_points_rdbm(:, 1, ipoint) = &
+               !         load_fw_points_rdbm(:, 1, ipoint) &
+               !             + 2 * mij_buff(i) * utemp(:,i)
+               !enddo 
+               !
+               !load_fw_points_rdbm(:, 1, ipoint) = &
+               !         + load_fw_points_rdbm(:, 1, ipoint)  &
+               !            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 1) 
+               
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(1) * utemp(:,1) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(2) * utemp(:,2) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(3) * utemp(:,3) &
+                            * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(4) * utemp(:,4) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 2) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(5) * utemp(:,5) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 1) 
+               load_fw_points_rdbm(:, 1, ipoint) = load_fw_points_rdbm(:, 1, ipoint) + mij_buff(6) * utemp(:,6) &
+                            * 2 * azim_factor_bw(rotmesh_phi(ipoint), (/0d0, 0d0, 1d0/), isim, 2) 
+
                write(6,*) '=================================================='
 
           case default
                stop
           end select
+         
+          !write(6,'(A,6E12.2)') 'input                ', mij_buff
+          !mij_buff = mij_buff * this%fwd(isim)%amplitude
+          !write(6,'(A,6E12.2)') 'scaled               ', mij_buff
+          !mij_buff = rotate_symm_tensor_voigt_src_to_xyz_1d(mij_buff, rotmesh_phi(ipoint))
+          !write(6,'(A,6E12.2)') 'src_to_xyz           ', mij_buff
+          !mij_buff = rotate_symm_tensor_voigt_xyz_src_to_xyz_earth_1d( &
+          !                      mij_buff, &
+          !                      reci_source_params%lon, &
+          !                      reci_source_params%colat)
+          !write(6,'(A,6E12.2)') 'xyz_src_to_xyz_earth ', mij_buff
+          !mij_buff = rotate_symm_tensor_voigt_xyz_earth_to_xyz_src_1d( &
+          !                      mij_buff, &
+          !                      source_params(ipoint)%lon, &
+          !                      source_params(ipoint)%colat)
+          !write(6,'(A,6E12.2)') 'xyz_earth_to_xyz_src ', mij_buff
+          !write(6,*) '=================================================='
        else
           stop
        endif
@@ -1203,15 +1346,15 @@ function load_strain_point(sem_obj, pointid, model_param)
         ! transform strain to voigt mapping
         ! from:
         ! ['strain_dsus', 'strain_dsuz', 'strain_dpup', &
-        !  'stroain_dsup', 'strain_dzup', 'straintrace']
+        !  'strain_dsup', 'strain_dzup', 'straintrace']
         ! to:
         ! dsus, dpup, dzuz, dzup, dsuz, dsup
         load_strain_point(:,1) = strain_buff(:,1)
         load_strain_point(:,2) = strain_buff(:,3)
         load_strain_point(:,3) = strain_buff(:,6) - strain_buff(:,1) - strain_buff(:,3)
-        load_strain_point(:,4) = strain_buff(:,5)
+        load_strain_point(:,4) = -strain_buff(:,5)
         load_strain_point(:,5) = strain_buff(:,2)
-        load_strain_point(:,6) = strain_buff(:,4)
+        load_strain_point(:,6) = -strain_buff(:,4)
     end select
 
 end function load_strain_point
