3a97a8878f9d1b44535a709ede09e1f46f203de5
sstaehler <staehler@geophysik.uni-muenchen.de>
2014-06-11 17:51:57 +0200

    Fix test_kernel (test cut_timewindows)
     - Test was crashing due to comparison of sp with dp variable

--- test_kernel.f90
+++ test_kernel.f90
@@ -13,7 +13,7 @@ contains
 subroutine test_kernel_cut_timewindow
    integer, parameter         :: ntimes = 100
    real(kind=dp)              :: timeseries(ntimes), dataseries(ntimes)
-   real(kind=dp)              :: dt = 0.1
+   real(kind=dp)              :: dt = 0.1d0
    real(kind=dp), allocatable :: cut_dataseries(:), cut_dataseries_ref(:)
    integer                    :: i
 
@@ -21,54 +21,59 @@ subroutine test_kernel_cut_timewindow
    do i=1, ntimes
       timeseries(i) = i * dt
       dataseries(i) = i 
+      ! print *, timeseries(i), dataseries(i)
    end do
 
    
+   ! Test 1: Time window bounds are between samples
    allocate(cut_dataseries_ref(6))
    cut_dataseries_ref = [15, 16, 17, 18, 19, 20]
 
    call cut_timewindow(timeseries, dataseries, [1.45d0, 2.01d0], cut_dataseries)
 
    call assert_comparable_real1d(real(cut_dataseries, kind=sp), real(cut_dataseries_ref, kind=sp), &
-                                 1e-9, 'cut_dataseries == cut_dataseries_ref')
+                                 1e-9, 'cut_dataseries == cut_dataseries_ref (Test 1)')
 
    deallocate(cut_dataseries_ref)
   
+   ! Test 2: Upper time window bound is exactly on sample
+   !         Correct behaviour would be that this sample is included in the resulting time window.
+   !         This test may fail due to floating point comparison, however, 2 should have an exact 
+   !         floating point representation 
    allocate(cut_dataseries_ref(6))
    cut_dataseries_ref = [15, 16, 17, 18, 19, 20]
 
    call cut_timewindow(timeseries, dataseries, [1.45d0, 2.0d0], cut_dataseries)
 
    call assert_comparable_real1d(real(cut_dataseries, kind=sp), real(cut_dataseries_ref, kind=sp), &
-                                 1e-9, 'cut_dataseries == cut_dataseries_ref')
-
+                                 1e-9, 'cut_dataseries == cut_dataseries_ref (Test 2)')
 
    deallocate(cut_dataseries_ref)
 
-   allocate(cut_dataseries_ref(6))
+   ! Test 3: Upper time window bound is narrowly (but within real(8) resolution) below sample
+   allocate(cut_dataseries_ref(5))
    cut_dataseries_ref = [15, 16, 17, 18, 19]
 
-   call cut_timewindow(timeseries, dataseries, [1.45d0, 1.99d0], cut_dataseries)
+   call cut_timewindow(timeseries, dataseries, [1.45d0, 1.9999999d0], cut_dataseries)
 
    call assert_comparable_real1d(real(cut_dataseries, kind=sp), real(cut_dataseries_ref, kind=sp), &
-                                 1e-9, 'cut_dataseries == cut_dataseries_ref')
+                                 1e-9, 'cut_dataseries == cut_dataseries_ref (Test 3)')
 
 
    deallocate(cut_dataseries_ref)
 
-   ! Time window with length 1
+   ! Test 4: Time window with length 1
    allocate(cut_dataseries_ref(1))
    cut_dataseries_ref = [80]
 
    call cut_timewindow(timeseries, dataseries, [7.95d0, 8.05d0], cut_dataseries)
 
    call assert_comparable_real1d(real(cut_dataseries, kind=sp), real(cut_dataseries_ref, kind=sp), &
-                                 1e-9, 'cut_dataseries == cut_dataseries_ref')
+                                 1e-9, 'cut_dataseries == cut_dataseries_ref (Test 4)' )
 
 
 
-   ! Time window with negative starting time
-
+   ! Test 5: Time window with negative starting time
    do i=1, ntimes
       timeseries(i) = i * dt - 5
       dataseries(i) = i - 50
@@ -82,7 +87,7 @@ subroutine test_kernel_cut_timewindow
    call cut_timewindow(timeseries, dataseries, [-0.39d0, 0.29d0], cut_dataseries)
 
    call assert_comparable_real1d(real(cut_dataseries, kind=sp), real(cut_dataseries_ref, kind=sp), &
-                                 1e-9, 'cut_dataseries == cut_dataseries_ref')
+                                 1e-9, 'cut_dataseries == cut_dataseries_ref (Test 5)')
 
 
 end subroutine test_kernel_cut_timewindow
