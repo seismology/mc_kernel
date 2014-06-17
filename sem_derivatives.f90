7fd10e131c4af6d2ca6f9b1b109d63a5dcb56de7
Martin van Driel <vandriel@erdw.ethz.ch>
2014-06-17 16:40:00 +0200

    adding vectorized version of dipole strain

--- sem_derivatives.f90
+++ sem_derivatives.f90
@@ -17,6 +17,11 @@ module sem_derivatives
     module procedure  :: strain_monopole_td
   end interface
 
+  interface strain_dipole
+    module procedure  :: strain_dipole
+    module procedure  :: strain_dipole_td
+  end interface
+
   interface f_over_s
     module procedure  :: f_over_s
     module procedure  :: f_over_s_td
@@ -200,6 +205,55 @@ end function
 !-----------------------------------------------------------------------------------------
 
 !--DIPOLE---------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
+function strain_dipole_td(u, G, GT, xi, eta, npol, nsamp, nodes, element_type, axial)
+  ! Computes the strain tensor for displacement u excited bz a monopole source
+  ! in Voigt notation: [dsus, dpup, dzuz, dzup, dsuz, dsup]
+  
+  integer, intent(in)           :: npol, nsamp
+  real(kind=dp), intent(in)     :: u(1:nsamp,0:npol,0:npol, 3)
+  real(kind=dp), intent(in)     :: G(0:npol,0:npol)  ! same for all elements (GLL)
+  real(kind=dp), intent(in)     :: GT(0:npol,0:npol) ! GLL for non-axial and GLJ for 
+                                                     ! axial elements
+  real(kind=dp), intent(in)     :: xi(0:npol)  ! GLL for non-axial and GLJ for axial 
+                                               ! elements
+  real(kind=dp), intent(in)     :: eta(0:npol) ! same for all elements (GLL)
+  real(kind=dp), intent(in)     :: nodes(4,2)
+  integer, intent(in)           :: element_type
+  logical, intent(in)           :: axial
+  real(kind=dp)                 :: strain_dipole_td(1:nsamp,0:npol,0:npol,6)
+  
+  real(kind=dp)                 :: grad_buff1(1:nsamp,0:npol,0:npol,2)
+  real(kind=dp)                 :: grad_buff2(1:nsamp,0:npol,0:npol,2)
+  real(kind=dp)                 :: grad_buff3(1:nsamp,0:npol,0:npol,2)
+  
+  ! 1: dsus, 2: dzus
+  grad_buff1 = axisym_gradient(u(:,:,:,1) + u(:,:,:,2), G, GT, xi, eta, npol, nsamp, &
+                               nodes, element_type)
+  
+  ! 1: dsup, 2: dzup
+  grad_buff2 = axisym_gradient(u(:,:,:,1) - u(:,:,:,2), G, GT, xi, eta, npol, nsamp, &
+                               nodes, element_type)
+
+  ! 1: dsuz, 2: dzuz
+  grad_buff3 = axisym_gradient(u(:,:,:,3), G, GT, xi, eta, npol, nsamp, nodes, &
+                               element_type)
+
+  strain_dipole_td(:,:,:,1) = grad_buff1(:,:,:,1)
+  strain_dipole_td(:,:,:,2) = 2 * f_over_s(u(:,:,:,2), G, GT, xi, eta, npol, nsamp, &
+                                           nodes, element_type, axial)
+  strain_dipole_td(:,:,:,3) = grad_buff3(:,:,:,2)
+  strain_dipole_td(:,:,:,4) = - 0.5d0 * (f_over_s(u(:,:,:,3), G, GT, xi, eta, npol, &
+                                                  nsamp, nodes, element_type, axial) &
+                                         + grad_buff2(:,:,:,2))
+  strain_dipole_td(:,:,:,5) = grad_buff1(:,:,:,2) + grad_buff3(:,:,:,1)
+  strain_dipole_td(:,:,:,6) = - f_over_s(u(:,:,:,2), G, GT, xi, eta, npol, nsamp, nodes, &
+                                  element_type, axial) &
+                              - grad_buff2(:,:,:,1) / 2d0
+
+end function
+!-----------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------
 function strain_dipole(u, G, GT, xi, eta, npol, nodes, element_type, axial)
