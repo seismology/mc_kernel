ca11517e24b0f685d7f707eb4ff41789f0c914a6
sstaehler <staehler@geophysik.uni-muenchen.de>
2014-07-26 18:31:09 +0200

    Parameter "ALLOWED_RELATIVE_ERROR"
    Allows to define a target relative error. Since kernel values range over several orders of magnitude, this can stop overly long calculations.

--- slave.f90
+++ slave.f90
@@ -313,7 +313,8 @@ function slave_work(parameters, sem_data, inv_mesh, fft_data) result(slave_resul
         do ibasisfunc = 1, nbasisfuncs_per_elem
             call int_kernel(ibasisfunc)%initialize_montecarlo(parameters%nkernel, &
                                                            volume,   &
-                                                           parameters%allowed_error) 
+                                                           parameters%allowed_error
+                                                           parameters%allowed_relative_error) 
         end do
       
         do while (.not.allallconverged(int_kernel)) ! Beginning of Monte Carlo loop
