09c5772f2885fc8aea8b7c2385db71a5e7349a64
martinvandriel <martin@vandriel.de>
2014-08-11 13:36:49 +0200

    adding test for source init on strike dip rake from srf file

--- test_source.f90
+++ test_source.f90
@@ -11,7 +11,7 @@ contains
 !-----------------------------------------------------------------------------------------
 subroutine test_read_srf
    character(len=256)   :: srf_file
-   integer              :: npoints, nsources
+   integer              :: npoints, nsources, i
    type(src_param_type), allocatable :: sources(:)
 
    srf_file = 'standard_rupture_fomat_testfile.srf'
@@ -19,6 +19,22 @@ subroutine test_read_srf
 
    call assert_equal(npoints, 10, 'Number of points')
    call assert_equal(nsources, 8, 'Number of sources')
+
+   !do i=1, nsources
+   !   write(6,'(6e10.2)') sources(i)%mij_voigt(:)
+   !enddo
+
+   call assert_comparable_real1d(real(sources(1)%mij_voigt, sp) + 1e22, &
+                                 [0., 0., 0., 0., 0., -0.64e22] + 1e22, 1e-5, &
+                                 'strike = 0, rake = 0, dip = 90')
+
+   call assert_comparable_real1d(real(sources(2)%mij_voigt, sp) + 1e22, &
+                                 [0.64e22, -0.64e22, 0., 0., 0., 0.] + 1e22, 1e-5, &
+                                 'strike = 0, rake = -45, dip = 90')
+
+   call assert_comparable_real1d(real(sources(3)%mij_voigt, sp) + 1e22, &
+                                 [0., 0., 0., 0., -0.64e22, 0.] + 1e22, 1e-5, &
+                                 'strike = 0, rake = 0, dip = 0')
 
 end subroutine
 !-----------------------------------------------------------------------------------------
