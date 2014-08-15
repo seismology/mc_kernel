aeba214d08812a5b477290914de47a453d76c745
martinvandriel <martin@vandriel.de>
2014-08-15 10:34:13 +0200

    tidy up

--- fft/make_fft.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gfortran -Wall -pedantic -fbacktrace -c -I $HOME/local/include/ -I /usr/include fft.f90
-gfortran fft.o -o fft -L $HOME/local/lib -Wl,-rpath,$HOME/local/lib -L /usr/lib -lm -lfftw3 -lfftw3f -Wall -pedantic -fbacktrace
