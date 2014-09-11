f38b9b0b56618ab93615594a3df3f7da0b610c3f
auerl <ludwig.auer@gmail.com>
2014-09-11 12:22:29 +0200

    slightly modified tests

--- test_readfields.f90
+++ test_readfields.f90
@@ -18,7 +18,7 @@ subroutine test_readfields_load_seismogram()
    call parameters%read_parameters('unit_tests/inparam_test')
    call parameters%read_source()
    call parameters%read_receiver()
-   call sem_data%set_params(parameters%fwd_dir, parameters%bwd_dir, 100)
+   call sem_data%set_params(parameters%fwd_dir, parameters%bwd_dir, 100, parameters%strain_type)
    call sem_data%open_files()
    call sem_data%read_meshes()
    call sem_data%load_seismogram(parameters%receiver, parameters%source)
@@ -47,12 +47,14 @@ end subroutine test_readfields_set_params
 !-----------------------------------------------------------------------------------------
 subroutine test_readfields_open_files()
    type(semdata_type)   :: sem_data
-   character(len=512)   :: fwd_dir, bwd_dir
+   character(len=512)   :: fwd_dir, bwd_dir, strain_type
+   
 
    fwd_dir = './wavefield/fwd'
    bwd_dir = './wavefield/bwd'
+   strain_type = 'strain_trace'
    
-   call sem_data%set_params(fwd_dir, bwd_dir, 100) 
+   call sem_data%set_params(fwd_dir, bwd_dir, 100, strain_type) 
 
    call sem_data%open_files()
 
