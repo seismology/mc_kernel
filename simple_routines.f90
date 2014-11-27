9ba166b7aaa88fbe12ca030f6fd75b67180fa30a
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2014-11-27 15:58:30 +0100

    Routine to check for NaNs
     - For 1D and 2D arrays (sp and dp), checks for NaNs and returns location of those
     - With interface, can be called as check_NaN
     - Interface for cross product function in same module

--- simple_routines.f90
+++ simple_routines.f90
@@ -2,6 +2,9 @@ module simple_routines
    use global_parameters, only: sp, dp
 
    implicit none
+   private
+   public mult2d_1d, mult3d_1d, absreldiff, check_NaN, cross
+   public to_lower, lowtrim, firstderiv
 
    interface mult2d_1d
       module procedure  :: mult2d_1d_dble
@@ -16,7 +19,20 @@ module simple_routines
    interface absreldiff
       module procedure  :: absreldiff_real
       module procedure  :: absreldiff_dble
-   end interface
+   end interface absreldiff
+
+   interface check_NaN
+     module procedure   :: check_NaN_1d_sp
+     module procedure   :: check_NaN_1d_dp
+     module procedure   :: check_NaN_2d_sp
+     module procedure   :: check_NaN_2d_dp
+   end interface check_NaN
+
+   interface cross
+     module procedure   :: cross_sp
+     module procedure   :: cross_dp
+   end interface cross
+  
 
    contains
 
@@ -104,7 +120,7 @@ integer                   :: ntimes
 
 ntimes = size(timeseries, 1)
 firstderiv       = 0
-firstderiv(2:99) = (timeseries(3:100) - timeseries(1:98)) / 2
+firstderiv(2:ntimes-1) = (timeseries(3:ntimes) - timeseries(1:ntimes-2)) / 2
 
 end function firstderiv
 !------------------------------------------------------------------------------
@@ -146,16 +162,133 @@ end function absreldiff_dble
 !------------------------------------------------------------------------------
 
 !------------------------------------------------------------------------------
-pure function cross(a,b)
+pure function cross_sp(a,b)
 !< Compute the cross product between vectors a and b
-  real(kind=dp)             :: cross(3)
+  real(kind=sp)             :: cross_sp(3)
+  real(kind=sp), intent(in) :: a(3), b(3)
+           
+  cross_sp(1) = a(2)*b(3) - a(3)*b(2)
+  cross_sp(2) = a(3)*b(1) - a(1)*b(3)
+  cross_sp(3) = a(1)*b(2) - a(2)*b(1)
+
+end function cross_sp
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+pure function cross_dp(a,b)
+!< Compute the cross product between vectors a and b
+  real(kind=dp)             :: cross_dp(3)
   real(kind=dp), intent(in) :: a(3), b(3)
            
-  cross(1) = a(2)*b(3) - a(3)*b(2)
-  cross(2) = a(3)*b(1) - a(1)*b(3)
-  cross(3) = a(1)*b(2) - a(2)*b(1)
+  cross_dp(1) = a(2)*b(3) - a(3)*b(2)
+  cross_dp(2) = a(3)*b(1) - a(1)*b(3)
+  cross_dp(3) = a(1)*b(2) - a(2)*b(1)
 
-end function cross
+end function cross_dp
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+!> Checks, whether any value of 'array' is NaN and returns its location
+!> 1D single precision version
+pure subroutine check_NaN_1d_sp(array, isnan, nan_loc)
+  real(kind=sp), intent(in)     :: array(:)    !< 1D array to check
+  logical,      intent(out)     :: isnan       !< Is any value of array NaN  
+  integer,      intent(out)     :: nan_loc     !< Location of first NaN in array
+  integer                       :: i1
+
+  nan_loc = 0
+  isnan = .false.
+
+  if (any(array.ne.array)) then
+    do i1 = 1, size(array,1)
+      if (array(i1).ne.array(i1)) then
+        nan_loc = i1
+        isnan = .true.
+      end if
+    end do
+  end if
+
+end subroutine check_NaN_1d_sp
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+!> Checks, whether any value of 'array' is NaN and returns its location
+!> 1D double precision version
+pure subroutine check_NaN_1d_dp(array, isnan, nan_loc)
+  real(kind=dp), intent(in)     :: array(:)    !< 1D array to check
+  logical,      intent(out)     :: isnan       !< Is any value of array NaN  
+  integer,      intent(out)     :: nan_loc     !< Location of first NaN in array
+  integer                       :: i1
+
+  nan_loc = 0
+  isnan = .false.
+
+  if (any(array.ne.array)) then
+    do i1 = 1, size(array,1)
+      if (array(i1).ne.array(i1)) then
+        nan_loc = i1
+        isnan = .true.
+      end if
+    end do
+  end if
+
+end subroutine check_NaN_1d_dp
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+!> Checks, whether any value of 'array' is NaN and returns its location
+!> 2D single precision version
+pure subroutine check_NaN_2d_sp(array, isnan, nan_loc)
+  real(kind=sp), intent(in)     :: array(:,:)  !< 2D array to check
+  logical,      intent(out)     :: isnan       !< Is any value of array NaN  
+  integer,      intent(out)     :: nan_loc(2)  !< Location of first NaN in array
+  integer                       :: i1, i2
+
+  nan_loc = [0, 0]
+  isnan = .false.
+
+  if (any(array.ne.array)) then
+    do i1 = 1, size(array,1)
+      do i2 = 1, size(array,2)
+        if (array(i1,i2).ne.array(i1,i2)) then
+          nan_loc = [i1, i2]
+          isnan = .true.
+          exit
+        end if
+      end do
+      if (isnan) exit
+    end do
+  end if
+
+end subroutine check_NaN_2d_sp
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+!> Checks, whether any value of 'array' is NaN and returns its location
+!> 2D double precision version
+pure subroutine check_NaN_2d_dp(array, isnan, nan_loc)
+  real(kind=dp), intent(in)     :: array(:,:)  !< 2D array to check
+  logical,      intent(out)     :: isnan       !< Is any value of array NaN  
+  integer,      intent(out)     :: nan_loc(2)  !< Location of first NaN in array
+  integer                       :: i1, i2
+
+  nan_loc = [0, 0]
+  isnan = .false.
+
+  if (any(array.ne.array)) then
+    do i1 = 1, size(array,1)
+      do i2 = 1, size(array,2)
+        if (array(i1,i2).ne.array(i1,i2)) then
+          nan_loc = [i1, i2]
+          isnan = .true.
+          exit
+        end if
+      end do
+      if (isnan) exit
+    end do
+  end if
+
+end subroutine check_NaN_2d_dp
 !------------------------------------------------------------------------------
 
 end module simple_routines
