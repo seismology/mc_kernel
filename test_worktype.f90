164ae9a4805c6e1b60aca967dafa5c869a27fa03
sstaehler <staehler@geophysik.uni-muenchen.de>
2015-08-28 12:19:05 +0200

    Test for worktype structure (just init)
     - Tests just the initialization. Should be amended later by wt communication test, which needs MPI testing

---
+++ test_worktype.f90
@@ -0,0 +1,96 @@
+!=========================================================================================
+module test_worktype
+
+  use global_parameters
+  use work_type_mod
+  use ftnunit
+# ifndef include_mpi
+  use mpi
+# endif
+  implicit none
+
+# ifdef include_mpi
+  include 'mpif.h'
+# endif
+  public
+contains
+
+
+!------------------------------------------------------------------------------
+subroutine test_init_work_type
+
+  integer           :: nkernel = 2
+  integer           :: nelems_per_task = 3
+  integer           :: nvertices_per_task = 10
+  integer           :: nvertices_per_elem = 4
+  integer           :: nbasisfuncs_per_elem = 1
+  integer           :: nmodel_parameters = 6
+  integer           :: nmodel_parameters_hetero = 6
+
+  logical           :: plot_wavefields = .true.
+  integer           :: ndumps = 100
+  integer           :: ndim = 6
+  real              :: dt = 1d-3
+
+
+  call init_work_type(nkernel                  = nkernel                  , &
+                      nelems_per_task          = nelems_per_task          , &
+                      nvertices                = nvertices_per_task       , &
+                      nvertices_per_elem       = nvertices_per_elem       , &
+                      nbasisfuncs_per_elem     = nbasisfuncs_per_elem     , &
+                      nmodel_parameters        = nmodel_parameters        , &
+                      nmodel_parameters_hetero = nmodel_parameters_hetero , &
+                      plot_wavefields          = plot_wavefields          , &
+                      ndumps                   = ndumps                   , &
+                      ndim                     = ndim                     , &
+                      dt                       = dt                       )
+  
+
+
+  ! Check values of parameters in resulting worktype
+  call assert_equal(wt%ntotal_kernel           , nkernel, 'nkernel')
+  call assert_equal(wt%nelems_per_task         , nelems_per_task, 'nelems_per_task')
+  call assert_equal(wt%nvertices               , nvertices_per_task, 'nvertices')
+  call assert_equal(wt%nvertices_per_elem      , nvertices_per_elem, 'nvertices_per_elem')
+  call assert_equal(wt%nbasisfuncs_per_elem    , nbasisfuncs_per_elem, 'nbasisfuncs_per_elem')
+  call assert_equal(wt%nmodel_parameters       , nmodel_parameters, 'nmodel_parameters')
+  call assert_equal(wt%nmodel_parameters_hetero, nmodel_parameters_hetero, 'nmodel_parameters_hetero')
+
+  call assert_equal(wt%ndumps                  , ndumps, 'ndumps')
+  call assert_equal(wt%ndim                    , ndim, 'ndim')
+  call assert_equal(wt%dt                      , nint(dt*1.e6) ,'dt in microseconds')
+
+
+  ! Check size of arrays in resulting worktype
+  call assert_equal(size(wt%connectivity),     nvertices_per_elem*nelems_per_task, &
+                    'size wt%connectivity')
+  call assert_equal(size(wt%vertices),         3 * nvertices_per_task, &
+                    'size wt%vertices')
+  call assert_equal(size(wt%kernel_values),    nelems_per_task * nkernel * nbasisfuncs_per_elem, &
+                    'size wt%kernel_values')
+  call assert_equal(size(wt%kernel_variance),  nelems_per_task * nkernel * nbasisfuncs_per_elem, &
+                    'size wt%kernel_variance')
+  call assert_equal(size(wt%niterations),      nelems_per_task * nkernel, &
+                    'size wt%niterations')
+  call assert_equal(size(wt%computation_time), nelems_per_task, &
+                    'size wt%computation_time')
+  call assert_equal(size(wt%model),            nelems_per_task * nmodel_parameters * nbasisfuncs_per_elem, &
+                    'size wt%model')
+  call assert_equal(size(wt%hetero_model),     nelems_per_task * nmodel_parameters_hetero * nbasisfuncs_per_elem, &
+                    'size wt%hetero_model')
+
+  call assert_equal(size(wt%fw_field),         ndumps * ndim * nkernel * nbasisfuncs_per_elem * nelems_per_task, &
+                    'size wt%fw_field')
+  call assert_equal(size(wt%bw_field),         ndumps * ndim * nkernel * nbasisfuncs_per_elem * nelems_per_task, &
+                    'size wt%bw_field')
+  call assert_equal(size(wt%conv_field),       ndumps * 1 * nkernel * nbasisfuncs_per_elem * nelems_per_task, &
+                    'size wt%conv_field')
+                    
+
+  call free_work_type()                 
+
+                    
+end subroutine test_init_work_type
+!------------------------------------------------------------------------------
+end module test_worktype
+
