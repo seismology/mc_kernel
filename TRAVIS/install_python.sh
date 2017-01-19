7bd999640066344f52f86ecd86b562ce996a1ac4
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2017-01-19 22:41:04 +0100

    Fix python package installation (use conda)

---
+++ TRAVIS/install_python.sh
@@ -0,0 +1,10 @@
+command -v conda >/dev/null || {
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+    bash miniconda.sh -b -f -p $MINICONDA;
+    conda update --yes conda;
+    conda remove --yes -n condaenv --all;
+    conda create --yes -n condaenv python=3.5;
+    conda install --yes -n condaenv pip;
+    source activate condaenv;
+    conda install --yes -c obspy obspy netcdf4 python=3.5
+  }
