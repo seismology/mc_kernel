878d87f819611dc1f851e3e3185d74a97af08555
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2017-01-20 09:26:38 +0100

    Update Conda environment each time
     - takes 2 minutes, but ensures clean environment

--- TRAVIS/install_python.sh
+++ TRAVIS/install_python.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 command -v conda >/dev/null || {
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -f -p $MINICONDA;
-    conda update --yes conda;
-    conda remove --yes -n condaenv --all;
-    conda create --yes -n condaenv python=3.5;
-    conda install --yes -n condaenv pip;
-    source activate condaenv;
-    conda install --yes -c obspy obspy h5py python=3.5 netcdf4 
   }
+conda update --yes conda;
+conda remove --yes -n condaenv --all;
+conda create --yes -n condaenv python=3.5;
+conda install --yes -n condaenv pip;
+source activate condaenv;
+conda install --yes -c obspy obspy h5py python=3.5 netcdf4 
