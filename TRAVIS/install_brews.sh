d332a0eef4b3dc3ab8c3f89b15176e3ac1022e08
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2017-01-28 20:45:53 +0100

    Explicit gcc installation

--- TRAVIS/install_brews.sh
+++ TRAVIS/install_brews.sh
@@ -5,11 +5,13 @@ brew update
 if [ -f "brew_cache.tar.gz" ]; then
   echo "Found Brew Cache, linking"
   tar zxf brew_cache.tar.gz --directory /usr/local/Cellar
-  brew link open-mpi gmp libmpc isl gcc || brew install open-mpi
+  brew link gcc
+  brew link open-mpi gmp libmpc isl || brew install open-mpi
   brew link fftw || brew install fftw --with-fortran
   brew link szip hdf5 netcdf || brew tap homebrew/science && brew install netcdf --with-fortran 
 else
   echo "Did not find Brew Cache, download and reinstall"
+  brew install gcc --with-fortran
   brew install open-mpi 
   brew install fftw --with-fortran
   brew tap homebrew/science
@@ -22,3 +24,4 @@ fi
 tar czf brew_cache.tar.gz --directory /usr/local/Cellar gmp libmpc isl gcc szip hdf5 open-mpi fftw netcdf
 ./gdrive upload brew_cache.tar.gz 
 
+brew doctor
